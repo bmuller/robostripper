d6a9d55f7ada222552a8d48cb040b12f241173e3
Andy Atkinson <andyatkinson@gmail.com>
2013-11-19 17:26:29 -0500

    Add some test coverage
     * a unit test for the HTTP headers to send
    
     * an integration test that hits the live page
    and adds a real YellowPage example from the
    documentation, with updates to the CSS selectors
    based on markup changes since the docs were written.
    
    Instead of a live page, mocha could be used to 
    serve a stub HTML response, but that wasn't done
    here.
    
    After bundling, run "rake test" to run the tests.

---
+++ test/robostripper_test.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'minitest/autorun'
+
+$:.unshift File.expand_path("../../lib", __FILE__)
+require 'robostripper'
+
+class YellowPage < Robostripper::Resource
+end
+YellowPage.add_item :details do
+  phone "p.phone"
+  street ".street-address"
+  city { scan(".city-state").split(",")[0] }
+  state { scan(".city-state").split(",")[1].scan(/[A-Z]{2}/)[0] }
+  zip { scan(".city-state").split(",")[1].scan(/\d{5}/)[0] }
+  address { [ street, city, state, zip ].join(" ") }
+  payments { scan(".payment").split(",").map(&:strip) }
+end
+
+class Robostripper::Test < MiniTest::Unit::TestCase
+  def test_http_headers
+    headers_hash = Robostripper::HTTP::HEADERS
+    assert_equal %w(Accept Accept-Charset Accept-Language User-Agent), headers_hash.keys
+    assert headers_hash["User-Agent"] =~ /Mozilla/
+  end
+
+  def test_yellowpage_resource_example
+    url = "http://www.yellowpages.com/silver-spring-md/mip/quarry-house-tavern-3342829"
+    yp = YellowPage.new(url)
+    assert_equal "(301) 587-8350", yp.details.phone
+    assert_equal "8401 Georgia Ave", yp.details.street
+    assert_equal "Silver Spring", yp.details.city
+    assert_equal "MD", yp.details.state
+    assert_equal "20910", yp.details.zip
+    assert_equal "8401 Georgia Ave Silver Spring MD 20910", yp.details.address
+    assert_equal ["master card", "discover", "amex", "visa"], yp.details.payments
+  end
+end
